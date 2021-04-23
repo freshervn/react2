cd9873756e23dde676222a0a4ed72b0725ee4093
thefagogroup <thefagogroup>
2021-04-23 12:41:59 +0700

    hope

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import linkAvatar from './images/l.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Đạt js</h1>
+        <img src={linkAvatar} className="App-logo" alt="logo" />        
+      </header>
+    </div>
+  );
+}
+
+export default App;
